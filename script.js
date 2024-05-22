8c5335b8b57ed1bbf7534b1c073c3aba7100c301
Lucho <51296478+gazaii@users.noreply.github.com>
2024-05-22 17:57:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // Obtener referencias a los elementos
 const contador = document.getElementById("contador");
 const btnActualizar = document.getElementById("btnActualizar");
- 
-// Variable para almacenar el valor del contador
-let count = 0;
- 
+
+// Cargar el valor del contador almacenado en localStorage
+let count = localStorage.getItem("contador") || 0;
+
 // Función para incrementar el contador
 function incrementarContador() {
-    count++;
-    contador.textContent = count;
+  count++;
+  contador.textContent = count;
+
+  // Actualizar el valor almacenado en localStorage
+  localStorage.setItem("contador", count);
 }
- 
+
 // Evento de click para el botón "Actualizar"
 btnActualizar.addEventListener("click", incrementarContador);
